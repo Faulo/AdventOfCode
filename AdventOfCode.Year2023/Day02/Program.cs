fe4d22069b86485171169c4ade39ac0fe2bb5c62
Daniel Schulz <faulolio@gmail.com>
2023-02-08 16:57:19 +0100

    Add Day02

---
+++ AdventOfCode.Year2023/Day02/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Day02;
+
+class Program {
+    const string INPUT_FOLDER = "input";
+
+    public static IEnumerable<string> ReadFileToArray(string file) {
+        return File.ReadLines(Path.Combine(INPUT_FOLDER, file));
+    }
+
+    public static int CalculateTotalScore(string file) {
+        return ReadFileToArray(file)
+            .Sum(CalculateLineScore);
+    }
+
+    public static int CalculateLineScore(string line) {
+        return CalculateLinePickScore(line) + CalculateLineWinScore(line);
+    }
+
+    const int SCORE_ROCK = 1;
+    const int SCORE_PAPER = 2;
+    const int SCORE_SCISSORS = 3;
+    public static int CalculateLinePickScore(string line) {
+        return line.Split(' ') switch {
+            [_, "X"] => SCORE_ROCK,
+            [_, "Y"] => SCORE_PAPER,
+            [_, "Z"] => SCORE_SCISSORS,
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    const int SCORE_WIN = 6;
+    const int SCORE_DRAW = 3;
+    const int SCORE_LOSS = 0;
+    public static int CalculateLineWinScore(string line) {
+        return line switch {
+            "A X" => SCORE_DRAW,
+            "A Y" => SCORE_WIN,
+            "A Z" => SCORE_LOSS,
+
+            "B X" => SCORE_LOSS,
+            "B Y" => SCORE_DRAW,
+            "B Z" => SCORE_WIN,
+
+            "C X" => SCORE_WIN,
+            "C Y" => SCORE_LOSS,
+            "C Z" => SCORE_DRAW,
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ AdventOfCode.Year2023/Day02/ProgramTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Day02;
+
+class ProgramTest {
+    [TestCase(
+        "example.txt",
+        "A Y",
+        "B X",
+        "C Z"
+    )]
+    public void TestReadFileToArray(string file, params string[] expected) {
+        CollectionAssert.AreEqual(expected, Program.ReadFileToArray(file));
+    }
+
+    [TestCase("A Y", 8)]
+    [TestCase("B X", 1)]
+    [TestCase("C Z", 6)]
+    public void TestCalculateLineScore(string line, int expected) {
+        Assert.AreEqual(expected, Program.CalculateLineScore(line));
+    }
+
+    [TestCase("A X", 1)]
+    [TestCase("A Y", 2)]
+    [TestCase("A Z", 3)]
+    public void TestCalculateLinePickScore(string line, int expected) {
+        Assert.AreEqual(expected, Program.CalculateLinePickScore(line));
+    }
+
+    [TestCase("A Y", 6)]
+    [TestCase("B X", 0)]
+    [TestCase("C Z", 3)]
+    public void TestCalculateLineWinScore(string line, int expected) {
+        Assert.AreEqual(expected, Program.CalculateLineWinScore(line));
+    }
+
+    [TestCase("example.txt", 15)]
+    public void TestCalculateTotalScore(string file, int expected) {
+        Assert.AreEqual(expected, Program.CalculateTotalScore(file));
+    }
+
+    [Test]
+    public void CalculateTotalScore() {
+        Assert.Pass($"Total Score: {Program.CalculateTotalScore("input.txt")}");
+    }
+}
