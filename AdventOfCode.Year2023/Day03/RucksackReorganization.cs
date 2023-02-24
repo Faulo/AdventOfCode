9f947333f5db5382b002d7f2d7737c6108143242
Daniel Schulz <faulolio@gmail.com>
2023-02-24 12:17:13 +0100

    Add RucksackReorganization

---
+++ AdventOfCode.Year2023/Day03/RucksackReorganization.cs
@@ -0,0 +1,28 @@
+﻿namespace Day03;
+
+class RucksackReorganization {
+    const string INPUT_FOLDER = "input";
+
+    public static IEnumerable<string> ReadFileToArray(string file) {
+        return File.ReadLines(Path.Combine(INPUT_FOLDER, file));
+    }
+
+    public static (string, string) DivideIntoCompartments(string line) {
+        return (line[..(line.Length / 2)], line[(line.Length / 2)..]);
+    }
+
+    public static char FindLetterThatAppearsInBothCompartments(string line) {
+        var (first, second) = DivideIntoCompartments(line);
+
+        for (int i = 0; i < first.Length; i++) {
+            char letter = first[i];
+            for (int j = 0; j < second.Length; j++) {
+                if (letter == second[j]) {
+                    return letter;
+                }
+            }
+        }
+
+        throw new Exception("No letter appeared in both!");
+    }
+}
---
+++ AdventOfCode.Year2023/Day03/RucksackReorganizationTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Day03;
+
+class RucksackReorganizationTest {
+    [TestCase("vJrwpWtwJgWrhcsFMMfFFhFp", "vJrwpWtwJgWr", "hcsFMMfFFhFp")]
+    public void TestDivideIntoCompartments(string line, string firstCompartment, string secondCompartment) {
+        Assert.AreEqual((firstCompartment, secondCompartment), RucksackReorganization.DivideIntoCompartments(line));
+    }
+
+    [TestCase("vJrwpWtwJgWrhcsFMMfFFhFp", 'p')]
+    public void TestFindLetterThatAppearsInBothCompartments(string line, char letterThatAppearsInBoth) {
+        Assert.AreEqual(letterThatAppearsInBoth, RucksackReorganization.FindLetterThatAppearsInBothCompartments(line));
+    }
+}
