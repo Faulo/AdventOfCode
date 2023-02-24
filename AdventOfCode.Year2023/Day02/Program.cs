2da176c4c39c56df805a662222c864ec5105b013
Daniel Schulz <faulolio@gmail.com>
2023-02-24 11:16:19 +0100

    Add Day03

---
+++ AdventOfCode.Year2023/Day02/Program.cs
@@ -0,0 +1,5 @@
+﻿using Day02;
+
+foreach (var format in new[] { MatchFormat.PickAndPick, MatchFormat.PickAndResult }) {
+    Console.WriteLine($"Total Score ({format}): {RockPaperScissors.CalculateTotalScore("input.txt", format)}");
+}
