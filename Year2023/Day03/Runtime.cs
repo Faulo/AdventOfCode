877f1854ec697ab543aeb5e0b58059ff6df6c4d3
Daniel Schulz <faulolio@gmail.com>
2023-12-03 12:48:24 +0100

    add fail

--- Year2023/Day03/Runtime.cs
+++ Year2023/Day03/Runtime.cs
@@ -1,18 +1,34 @@
-﻿
-using Utilities;
+﻿using Utilities;
 
 namespace Day03;
 
 sealed class Runtime {
     readonly char[,] map;
+    internal readonly int width;
+    internal readonly int height;
 
-    public bool sumOfAdjacentParts { get; internal set; }
+    internal int sumOfAdjacentParts {
+        get {
+            int sum = 0;
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    if (TryGetAdjacentPart(x, y, out int part)) {
+                        sum += part;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
 
     internal Runtime(string file) {
         map = new FileInput(file).ReadAllCharactersAsMap();
+        width = map.GetLength(0);
+        height = map.GetLength(1);
     }
 
-    internal bool TryGet(int x, int y, out char symbol) {
+    internal bool TryGetSymbol(int x, int y, out char symbol) {
         if (!IsValid(x, y)) {
             symbol = default;
             return false;
@@ -22,7 +38,53 @@ sealed class Runtime {
         return true;
     }
 
+    internal bool TryGetAdjacentPart(int x, int y, out int number) {
+        if (TryGetSymbol(x - 1, y, out char symbol) && char.IsDigit(symbol)) {
+            number = default;
+            return false;
+        }
+
+        string result = "";
+        bool isAdjacent = false;
+        for (; x < width; x++) {
+            if (!isAdjacent) {
+                isAdjacent = IsAdjacent(x, y);
+            }
+
+            if (!TryGetSymbol(x, y, out symbol)) {
+                break;
+            }
+
+            if (char.IsDigit(symbol)) {
+                result += symbol;
+            } else {
+                break;
+            }
+        }
+
+        number = result == ""
+            ? default
+            : int.Parse(result);
+
+        return isAdjacent;
+    }
+
     bool IsValid(int x, int y)
-        => x >= 0 && x < map.GetLength(0)
-        && y >= 0 && y < map.GetLength(1);
+        => x >= 0 && x < width
+        && y >= 0 && y < height;
+
+    bool IsAdjacent(int x, int y)
+        => IsPart(x + 0, y + 1)
+        || IsPart(x + 1, y + 0)
+        || IsPart(x + 1, y + 1)
+        || IsPart(x + 0, y - 1)
+        || IsPart(x - 1, y + 0)
+        || IsPart(x - 1, y - 1)
+        || IsPart(x + 1, y - 1)
+        || IsPart(x - 1, y + 1);
+
+    bool IsPart(int x, int y)
+        => TryGetSymbol(x, y, out char symbol)
+        && symbol != '.'
+        && !char.IsDigit(symbol);
 }
--- Year2023/Day03/Tests.cs
+++ Year2023/Day03/Tests.cs
@@ -10,6 +10,12 @@ public class Tests {
 
         Assert.That(runtime.sumOfAdjacentParts, Is.EqualTo(expected));
     }
+    [TestCase("input.txt", 538120)]
+    public void GivenSchematic_WhenCalculateSumOfParts_ThenReturnLess(string file, int expected) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.sumOfAdjacentParts, Is.LessThan(expected));
+    }
 
     [TestCase("example-1.txt", 0, 0, '4')]
     [TestCase("example-1.txt", 1, 0, '6')]
@@ -17,7 +23,7 @@ public class Tests {
     public void GivenSchematic_WhenTryGet_ThenReturnSymbol(string file, int x, int y, char expected) {
         var runtime = new Runtime(file);
 
-        Assert.That(runtime.TryGet(x, y, out char actual), Is.True);
+        Assert.That(runtime.TryGetSymbol(x, y, out char actual), Is.True);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
@@ -29,6 +35,35 @@ public class Tests {
     public void GivenSchematic_WhenTryGetInvalidCoord_ThenReturnFalse(string file, int x, int y) {
         var runtime = new Runtime(file);
 
-        Assert.That(runtime.TryGet(x, y, out _), Is.False);
+        Assert.That(runtime.TryGetSymbol(x, y, out _), Is.False);
+    }
+
+    [TestCase("example-1.txt", 0, 0, 467)]
+    [TestCase("example-1.txt", 2, 2, 35)]
+    [TestCase("example-1.txt", 6, 2, 633)]
+    public void GivenSchematic_WhenTryGetAdjacentPart_ThenReturnAdjacentPart(string file, int x, int y, int expected) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.TryGetAdjacentPart(x, y, out int actual), Is.True);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("example-1.txt", 5, 0)]
+    [TestCase("example-1.txt", 1, 0)]
+    [TestCase("example-1.txt", 2, 0)]
+    [TestCase("example-1.txt", 3, 2)]
+    public void GivenSchematic_WhenTryGetAdjacentPartInvalidCoord_ThenReturnFalse(string file, int x, int y) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.TryGetAdjacentPart(x, y, out _), Is.False);
+    }
+
+    [TestCase("example-1.txt", 10, 10)]
+    [TestCase("input.txt", 140, 140)]
+    public void GivenSchematic_WhenTryGetSize_ThenReturnWidthAndHeight(string file, int width, int height) {
+        var runtime = new Runtime(file);
+
+        Assert.That((runtime.width, runtime.height), Is.EqualTo((width, height)));
     }
 }
