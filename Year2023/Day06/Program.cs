8dbb3953e9d174b9c965063489e88f68723a1b8f
Daniel Schulz <faulolio@gmail.com>
2023-12-06 09:26:04 +0100

    Solve

--- Year2023/Day06/Program.cs
+++ Year2023/Day06/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine($"{nameof(Runtime.productOfWins)} example-1.txt: {new Runtime("example-1.txt").productOfWins}");
 
 Console.WriteLine($"{nameof(Runtime.productOfWins)} input.txt: {new Runtime("input.txt").productOfWins}");
+
+Console.WriteLine($"{nameof(Runtime.productOfWins)} example-1.txt: {new Runtime("example-1.txt", true).productOfWins}");
+
+Console.WriteLine($"{nameof(Runtime.productOfWins)} input.txt: {new Runtime("input.txt", true).productOfWins}");
--- Year2023/Day06/Runtime.cs
+++ Year2023/Day06/Runtime.cs
@@ -4,10 +4,32 @@ using Utilities;
 namespace Day06;
 
 sealed class Runtime {
-    internal record Race(int time, int distance) {
-        internal int wins {
+    internal record Race(long time, long distance) {
+        internal long wins {
             get {
-                return 0;
+                return 1 + lastWin - firstWin;
+            }
+        }
+        internal long firstWin {
+            get {
+                for (long i = 1; i < time; i++) {
+                    if (i * (time - i) > distance) {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+        }
+        internal long lastWin {
+            get {
+                for (long i = time - 1; i > 0; i--) {
+                    if (i * (time - i) > distance) {
+                        return i;
+                    }
+                }
+
+                return -1;
             }
         }
     }
@@ -15,20 +37,28 @@ sealed class Runtime {
     internal readonly List<Race> races = [];
     readonly string file;
 
-    public int productOfWins => races
-        .Aggregate(1, (p, r) => r.wins * p);
+    public long productOfWins => races
+        .Aggregate((long)1, (p, r) => r.wins * p);
 
-    internal Runtime(string file) {
+    internal Runtime(string file, bool badKerning = false) {
         this.file = file;
 
         var lines = new FileInput(file).ReadLines();
-        var times = Regex.Matches(lines.First(), "(\\d+)");
-        var distances = Regex.Matches(lines.Last(), "(\\d+)");
+        string first = lines.First();
+        string last = lines.Last();
+
+        if (badKerning) {
+            first = first.Replace(" ", "");
+            last = last.Replace(" ", "");
+        }
+
+        var times = Regex.Matches(first, "(\\d+)");
+        var distances = Regex.Matches(last, "(\\d+)");
 
         for (int i = 0; i < times.Count; i++) {
             races.Add(new(
-                int.Parse(times[i].Groups[1].Value),
-                int.Parse(distances[i].Groups[1].Value)
+                long.Parse(times[i].Groups[1].Value),
+                long.Parse(distances[i].Groups[1].Value)
            ));
         }
     }
