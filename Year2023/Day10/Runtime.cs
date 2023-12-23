40de80993ce0e6bfdb13269f4c46dbaaeeaabe53
Daniel Schulz <faulolio@gmail.com>
2023-12-23 14:06:28 +0100

    feat: solve 10-1

--- Year2023/Day10/Runtime.cs
+++ Year2023/Day10/Runtime.cs
@@ -1,11 +1,136 @@
-﻿namespace Day10;
+﻿using Utilities;
+
+namespace Day10;
 
 sealed class Runtime {
-    readonly string file;
+    char this[Vector2 position] {
+        get {
+            return map[position.x, position.y];
+        }
+    }
 
-    internal int maximumDistance => 0;
+    readonly char[,] map;
+    int width => map.GetLength(0);
+    int height => map.GetLength(1);
+
+    internal int maximumDistance {
+        get {
+            var paths = GetNeighborsPointingTo(start)
+                .ToList();
+            var first = paths[0];
+            var second = paths[1];
+
+            int i = 1;
+
+            var previousFirst = start;
+            var previousSecond = start;
+            while (first != second) {
+                i++;
+
+                var newFirst = GetNeighborsPointingFrom(first).First(p => p != previousFirst);
+                var newSecond = GetNeighborsPointingFrom(second).First(p => p != previousSecond);
+
+                previousFirst = first;
+                previousSecond = second;
+
+                first = newFirst;
+                second = newSecond;
+            }
+
+            return i;
+        }
+    }
+
+    internal Vector2 start {
+        get {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    if (map[x, y] == 'S') {
+                        return new(x, y);
+                    }
+                }
+            }
+
+            throw new Exception();
+        }
+    }
 
     internal Runtime(string file) {
-        this.file = file;
+        map = new FileInput(file).ReadAllCharactersAsMap();
+    }
+
+    internal IEnumerable<Vector2> GetNeighborsPointingTo(Vector2 position) {
+        foreach (var neighbor in GetNeighbors(position)) {
+            if (this[neighbor].GetDirections().GetOffsets().Any(offset => offset == position - neighbor)) {
+                yield return neighbor;
+            }
+        }
+    }
+
+    internal IEnumerable<Vector2> GetNeighborsPointingFrom(Vector2 position) {
+        return this[position]
+            .GetDirections()
+            .GetOffsets()
+            .Select(offset => position + offset);
+    }
+
+    IEnumerable<Vector2> GetNeighbors(Vector2 position) {
+        for (int x = position.x - 1; x <= position.x + 1; x++) {
+            for (int y = position.y - 1; y <= position.y + 1; y++) {
+                var neighbor = new Vector2(x, y);
+                if (IsInBounds(neighbor) && neighbor != position) {
+                    yield return neighbor;
+                }
+            }
+        }
+    }
+    bool IsInBounds(Vector2 position) {
+        return position.x >= 0 && position.x < width
+            && position.y >= 0 && position.y < height;
+    }
+}
+
+enum Directions {
+    None = 0,
+    North = 1 << 0,
+    East = 1 << 1,
+    South = 1 << 2,
+    West = 1 << 3,
+}
+
+sealed record Vector2(int x, int y) {
+    public static Vector2 operator +(Vector2 a, Vector2 b) => new(a.x + b.x, a.y + b.y);
+    public static Vector2 operator -(Vector2 a, Vector2 b) => new(a.x - b.x, a.y - b.y);
+}
+
+static class Extensions {
+    internal static Directions GetDirections(this char pipe) {
+        return pipe switch {
+            '|' => Directions.North | Directions.South, // is a vertical pipe connecting north and south.
+            '-' => Directions.East | Directions.West, // is a horizontal pipe connecting east and west.
+            'L' => Directions.North | Directions.East, // is a 90 - degree bend connecting north and east.
+            'J' => Directions.North | Directions.West, // is a 90 - degree bend connecting north and west.
+            '7' => Directions.South | Directions.West, // is a 90 - degree bend connecting south and west.
+            'F' => Directions.South | Directions.East, // is a 90-degree bend connecting south and east.
+            _ => Directions.None,
+        };
+    }
+
+    internal static IEnumerable<Vector2> GetOffsets(this Directions directions) {
+        if (directions.HasFlag(Directions.North)) {
+            yield return new(0, -1);
+        }
+
+        if (directions.HasFlag(Directions.East)) {
+            yield return new(1, 0);
+        }
+
+        if (directions.HasFlag(Directions.South)) {
+            yield return new(0, 1);
+        }
+
+        if (directions.HasFlag(Directions.West)) {
+            yield return new(-1, 0);
+        }
     }
 }
--- Year2023/Day10/Tests.cs
+++ Year2023/Day10/Tests.cs
@@ -11,4 +11,32 @@ public class Tests {
 
         Assert.That(runtime.maximumDistance, Is.EqualTo(expected));
     }
+
+    [TestCase("example-1.txt", 1, 1)]
+    [TestCase("example-2.txt", 0, 2)]
+    public void Test_Runtime_Start(string file, int expectedX, int expectedY) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.start, Is.EqualTo(new Vector2(expectedX, expectedY)));
+    }
+
+    [TestCase("example-1.txt", 1, 1, 2, 1)]
+    [TestCase("example-1.txt", 1, 1, 1, 2)]
+    [TestCase("example-2.txt", 0, 2, 1, 2)]
+    [TestCase("example-2.txt", 0, 2, 0, 3)]
+    public void Test_Runtime_GetNeighborsPointingTo(string file, int x, int y, int expectedX, int expectedY) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.GetNeighborsPointingTo(new Vector2(x, y)), Contains.Item(new Vector2(expectedX, expectedY)));
+    }
+
+    [TestCase("example-1.txt", 2, 1, 1, 1)]
+    [TestCase("example-1.txt", 1, 2, 1, 1)]
+    [TestCase("example-2.txt", 1, 2, 0, 2)]
+    [TestCase("example-2.txt", 0, 3, 0, 2)]
+    public void Test_Runtime_GetNeighborsPointingFrom(string file, int x, int y, int expectedX, int expectedY) {
+        var runtime = new Runtime(file);
+
+        Assert.That(runtime.GetNeighborsPointingFrom(new Vector2(x, y)), Contains.Item(new Vector2(expectedX, expectedY)));
+    }
 }
