0551af07df81d5087a288306d82b752ed76bb2fa
Daniel Schulz <faulolio@gmail.com>
2023-12-31 15:26:33 +0100

    feat: use struct for Vector2Int

--- Year2023/Utilities/Vector2Int.cs
+++ Year2023/Utilities/Vector2Int.cs
@@ -1,5 +1,5 @@
 ﻿namespace Utilities {
-    public sealed record Vector2Int(int x, int y) {
+    public readonly struct Vector2Int {
         /// <summary>
         /// (0, 0)
         /// </summary>
@@ -27,6 +27,14 @@
 
         public Vector2Int((int x, int y) position) : this(position.x, position.y) { }
 
+        public readonly int x;
+        public readonly int y;
+
+        public Vector2Int(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
         public int manhattenDistance => x + y;
 
         public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new(a.x + b.x, a.y + b.y);
@@ -34,5 +42,18 @@
 
         public static Vector2Int operator *(Vector2Int a, int b) => new(a.x * b, a.y * b);
         public static Vector2Int operator *(int b, Vector2Int a) => new(a.x * b, a.y * b);
+
+        public static bool operator ==(Vector2Int a, Vector2Int b) => a.x == b.x && a.y == b.y;
+        public static bool operator !=(Vector2Int a, Vector2Int b) => a.x != b.x || a.y != b.y;
+
+        public override bool Equals(object? obj) {
+            return obj is Vector2Int other && other == this;
+        }
+
+        public override int GetHashCode() {
+            return (x << 16) | y;
+        }
+
+        public override string ToString() => $"({x}, {y})";
     }
 }
