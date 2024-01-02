1c531be851b54a915e3af94713f7474cce077257
Daniel Schulz <faulolio@gmail.com>
2024-01-02 15:56:25 +0100

    feat: solve 20-1!

--- Year2023/Day20/Runtime.cs
+++ Year2023/Day20/Runtime.cs
@@ -1,22 +1,62 @@
-﻿using Utilities;
+﻿using NUnit.Framework;
+using Utilities;
+using Pulse = (string source, string target, bool isHigh);
 
 namespace Day20;
 
 sealed class Runtime {
     internal long productOfPulses {
         get {
-            long sum = 0;
+            var (lowSum, highSum) = pulseCounts;
 
-            return sum;
+            return lowSum * highSum;
+        }
+    }
+
+    internal (long low, long high) pulseCounts {
+        get {
+            long lowSum = 0;
+            long highSum = 0;
+
+            var queue = new Queue<Pulse>();
+
+            for (int i = 0; i < 1000; i++) {
+                queue.Enqueue(("button", "broadcaster", false));
+
+                do {
+                    var pulse = queue.Dequeue();
+
+                    if (pulse.isHigh) {
+                        highSum++;
+                    } else {
+                        lowSum++;
+                    }
+
+                    if (modules.TryGetValue(pulse.target, out var module)) {
+                        foreach (var output in module.SendPulse(pulse)) {
+                            queue.Enqueue(output);
+                        }
+                    }
+                } while (queue.Count > 0);
+            }
+
+            return (lowSum, highSum);
         }
     }
 
     internal readonly Dictionary<string, Module> modules = [];
 
     internal Runtime(string file) {
+        var button = new Module("button", "broadcaster");
+        modules[button.name] = button;
+
         foreach (string line in new FileInput(file).ReadLines()) {
             var module = Module.Parse(line);
             modules[module.name] = module;
+        }
+
+        foreach (var module in modules.Values) {
+            module.LinkModules(modules);
         }
     }
 }
@@ -32,6 +72,13 @@ class Module {
         this.outputs = outputs;
     }
 
+    internal virtual IEnumerable<Pulse> SendPulse(Pulse pulse) {
+        return outputs.Select(m => (name, m, pulse.isHigh));
+    }
+
+    internal virtual void LinkModules(IDictionary<string, Module> modules) {
+    }
+
     internal static Module Parse(string line) {
         string[] values = line.Split("->");
         string name = values[0].Trim();
@@ -41,8 +88,8 @@ class Module {
             .ToArray();
 
         return name[0] switch {
-            '%' => new FlipFlopModule(name, outputs),
-            '&' => new ConjunctionModule(name, outputs),
+            '%' => new FlipFlopModule(name[1..], outputs),
+            '&' => new ConjunctionModule(name[1..], outputs),
             _ => new Module(name, outputs)
         };
     }
@@ -51,12 +98,47 @@ class Module {
 class FlipFlopModule : Module {
     internal FlipFlopModule(string name, params string[] outputs) : base(name, outputs) {
     }
+
+    internal bool isOn;
+
+    internal override IEnumerable<Pulse> SendPulse(Pulse pulse) {
+        if (pulse.isHigh) {
+            return Enumerable.Empty<Pulse>();
+        }
+
+        isOn = !isOn;
+
+        pulse.isHigh = isOn;
+
+        return base.SendPulse(pulse);
+    }
 }
 
 class ConjunctionModule : Module {
+    readonly Dictionary<string, bool> inputs = [];
     internal ConjunctionModule(string name, params string[] outputs) : base(name, outputs) {
+    }
+
+    internal override void LinkModules(IDictionary<string, Module> modules) {
+        foreach (var (name, module) in modules) {
+            if (module.outputs.Contains(this.name)) {
+                inputs[name] = false;
+            }
+        }
+    }
+
+    internal override IEnumerable<Pulse> SendPulse(Pulse pulse) {
+        Assert.That(inputs, Contains.Key(pulse.source));
+        inputs[pulse.source] = pulse.isHigh;
+
+        pulse.isHigh = inputs.Values.Any(i => !i);
+
+        return base.SendPulse(pulse);
     }
 }
 
 static class Extensions {
+    internal static Pulse CreatePulse(this bool isHigh, Module source, Module target) {
+        return (source.name, target.name, isHigh);
+    }
 }
--- Year2023/Day20/Tests.cs
+++ Year2023/Day20/Tests.cs
@@ -12,11 +12,134 @@ public class Tests {
         Assert.That(sut.productOfPulses, Is.EqualTo(expected));
     }
 
-    [TestCase("example-1.txt", 5)]
-    [TestCase("example-2.txt", 5)]
-    public void Test_Module_Count(string file, long expected) {
+    [TestCase("example-1.txt", 8000, 4000)]
+    [TestCase("example-2.txt", 4250, 2750)]
+    public void Test_Runtime_PulseCounts(string file, long expectedLow, long expectedHigh) {
+        var sut = new Runtime(file);
+
+        var expected = (expectedLow, expectedHigh);
+
+        Assert.That(sut.pulseCounts, Is.EqualTo(expected));
+    }
+
+    [TestCase("example-1.txt", 6)]
+    [TestCase("example-2.txt", 6)]
+    public void Test_Runtime_Module_Count(string file, long expected) {
         var sut = new Runtime(file);
 
         Assert.That(sut.modules.Count, Is.EqualTo(expected));
+    }
+
+    [TestCase("example-1.txt", 0)]
+    [TestCase("example-1.txt", 1)]
+    [TestCase("example-1.txt", 2)]
+    [TestCase("example-1.txt", 3)]
+    [TestCase("example-1.txt", 4)]
+    public void Test_Runtime_FlipFlopsAreOff(string file, int buttonCount) {
+        var sut = new Runtime(file);
+
+        for (int i = 0; i < buttonCount; i++) {
+            _ = sut.pulseCounts;
+        }
+
+        foreach (var (name, module) in sut.modules) {
+            if (module is FlipFlopModule flipFlop) {
+                Assert.That(flipFlop.isOn, Is.EqualTo(false), $"FlipFlop '{name}' was on?!");
+            }
+        }
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Test_Module_SendPulse(bool isHigh) {
+        var broadcaster = new Module("broadcaster", "test");
+        var sut = new Module("test", "a", "b");
+
+        var pulse = isHigh.CreatePulse(broadcaster, sut);
+
+        var expected = new[] {
+            (sut.name, "a", isHigh),
+            (sut.name, "b", isHigh),
+        };
+
+        Assert.That(sut.SendPulse(pulse), Is.EqualTo(expected));
+    }
+
+    [TestCase(false, true, false, false)]
+    [TestCase(true, true, false, false)]
+    [TestCase(false, false, true, true)]
+    [TestCase(true, false, true, false)]
+    public void Test_FlipFlopModule_SendPulse(bool initialState, bool isHigh, bool expectOutput, bool expectedPulse) {
+        var broadcaster = new Module("broadcaster", "test");
+        var sut = new FlipFlopModule("test", "a", "b") {
+            isOn = initialState
+        };
+
+        var expected = expectOutput
+            ? new[] {
+                (sut.name, "a", expectedPulse),
+                (sut.name, "b", expectedPulse),
+            }
+            : Array.Empty<(string, string, bool)>();
+
+        var pulse = isHigh.CreatePulse(broadcaster, sut);
+        Assert.That(sut.SendPulse(pulse), Is.EqualTo(expected));
+    }
+
+    [TestCase(0, false)]
+    [TestCase(1, true)]
+    [TestCase(2, false)]
+    [TestCase(3, true)]
+    public void Test_FlipFlopModule_SendPulses(int count, bool expected) {
+        var broadcaster = new Module("broadcaster", "test");
+        var sut = new FlipFlopModule("test", "a", "b");
+
+        var pulse = false.CreatePulse(broadcaster, sut);
+        for (int i = 0; i < count; i++) {
+            sut.SendPulse(pulse);
+        }
+
+        Assert.That(sut.isOn, Is.EqualTo(expected));
+    }
+
+    [TestCase(false, true)]
+    [TestCase(true, false)]
+    public void Test_Conjunction_SendPulse(bool isHigh, bool expectedPulse) {
+        var broadcaster = new Module("broadcaster", "test");
+        var sut = new ConjunctionModule("test", "a", "b");
+        sut.LinkModules(new Dictionary<string, Module> { [broadcaster.name] = broadcaster });
+
+        var expected = new[] {
+            (sut.name, "a", expectedPulse),
+            (sut.name, "b", expectedPulse),
+        };
+
+        var pulse = isHigh.CreatePulse(broadcaster, sut);
+        Assert.That(sut.SendPulse(pulse), Is.EqualTo(expected));
+    }
+
+    [TestCase(false, false, true)]
+    [TestCase(false, true, true)]
+    [TestCase(true, false, true)]
+    [TestCase(true, true, false)]
+    public void Test_Conjunction_SendPulses(bool aHigh, bool bHigh, bool expectedPulse) {
+        var a = new Module("a", "test");
+        var b = new Module("b", "test");
+        var sut = new ConjunctionModule("test", "a", "b");
+        sut.LinkModules(new Dictionary<string, Module> { [a.name] = a, [b.name] = b });
+
+        var expectedA = new[] {
+            (sut.name, "a", true),
+            (sut.name, "b", true),
+        };
+        var expectedB = new[] {
+            (sut.name, "a", expectedPulse),
+            (sut.name, "b", expectedPulse),
+        };
+
+        var pulseA = aHigh.CreatePulse(a, sut);
+        var pulseB = bHigh.CreatePulse(b, sut);
+        Assert.That(sut.SendPulse(pulseA), Is.EqualTo(expectedA));
+        Assert.That(sut.SendPulse(pulseB), Is.EqualTo(expectedB));
     }
 }
