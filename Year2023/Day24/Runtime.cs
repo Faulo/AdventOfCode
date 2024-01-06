6505820dad33bccaef176e11d20032b29867aba1
Daniel Schulz <faulolio@gmail.com>
2024-01-06 19:58:01 +0100

    feat: try solve 24-1

--- Year2023/Day24/Runtime.cs
+++ Year2023/Day24/Runtime.cs
@@ -1,12 +1,54 @@
-﻿using Utilities;
+﻿using NUnit.Framework;
+using Utilities;
 
 namespace Day24;
 
 sealed class Runtime {
+    internal static bool TryCalculateIntersection(Hailstone h1, Hailstone h2, out (long x, long y) position) {
+        long determinant = (h1.line.x * h2.line.y) - (h2.line.x * h1.line.y);
+
+        if (determinant == 0) {
+            position = default;
+            return false;
+        }
+
+        long x = ((h2.line.y * h1.line.z) - (h1.line.y * h2.line.z)) / determinant;
+        long y = ((h1.line.x * h2.line.z) - (h2.line.x * h1.line.z)) / determinant;
+        position = (x, y);
+
+        var delta1 = position;
+        delta1.x -= h1.position.x;
+        delta1.y -= h1.position.y;
+
+        var delta2 = position;
+        delta2.x -= h2.position.x;
+        delta2.y -= h2.position.y;
+
+        // Console.WriteLine($"{Math.Sign(h1.velocity.x) == Math.Sign(delta1.x)} && {Math.Sign(h1.velocity.y) == Math.Sign(delta1.y)} && {Math.Sign(h2.velocity.x) == Math.Sign(delta2.x)} && {Math.Sign(h2.velocity.y) == Math.Sign(delta2.y)}");
+
+        return Math.Sign(h1.velocity.x) == Math.Sign(delta1.x)
+            && Math.Sign(h1.velocity.y) == Math.Sign(delta1.y)
+            && Math.Sign(h2.velocity.x) == Math.Sign(delta2.x)
+            && Math.Sign(h2.velocity.y) == Math.Sign(delta2.y);
+    }
+
     internal readonly List<Hailstone> hailstones = [];
 
     internal int GetNumberOfCollisions(long min, long max) {
-        return hailstones.Count;
+        int count = 0;
+        for (int i = 0; i < hailstones.Count; i++) {
+            for (int j = i + 1; j < hailstones.Count; j++) {
+                var h1 = hailstones[i];
+                var h2 = hailstones[j];
+                if (TryCalculateIntersection(h1, h2, out var position)) {
+                    if (position.x >= min && position.y >= min && position.x <= max && position.y <= max) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
     }
 
     internal Runtime(string file) {
@@ -17,8 +59,26 @@ sealed class Runtime {
 }
 
 sealed class Hailstone {
-    internal Vector3Int position { get; private set; }
-    internal Vector3Int velocity { get; private set; }
+    internal readonly Vector3Int position;
+    internal readonly Vector3Int velocity;
+    internal readonly Vector3Int line;
+
+    internal Hailstone(Vector3Int position, Vector3Int velocity) {
+        Assert.That(velocity.x, Is.Not.EqualTo(0));
+        Assert.That(velocity.y, Is.Not.EqualTo(0));
+
+        this.position = position;
+        this.velocity = velocity;
+
+        var p1 = position;
+        var p2 = position + velocity;
+        line.x = p2.y - p1.y;
+        line.y = p1.x - p2.x;
+        line.z = (line.x * p1.x) + (line.y * p1.y);
+        //Assert.That(line.z, Is.EqualTo(((double)line.x * p1.x) + ((double)line.y * p1.y)), $"{line.x} * {p1.x} + {line.y} * {p1.y}");
+    }
+
+    public override string ToString() => line.ToString();
 
     internal static Hailstone Parse(string line) {
         var positions = line
@@ -27,10 +87,7 @@ sealed class Hailstone {
             .Select(long.Parse)
             .ToList();
 
-        return new() {
-            position = new(positions[0], positions[1], positions[2]),
-            velocity = new(positions[3], positions[4], positions[5]),
-        };
+        return new(new(positions[0], positions[1], positions[2]), new(positions[3], positions[4], positions[5]));
     }
 }
 
--- Year2023/Day24/Tests.cs
+++ Year2023/Day24/Tests.cs
@@ -5,11 +5,19 @@ namespace Day24;
 
 [TestFixture(TestOf = typeof(Runtime))]
 public class Tests {
+    const float ERROR_MARGIN = 1023f / 1024;
+
     [TestCase("example-1.txt", 7, 27, 2)]
-    public void Test_Runtime_MaximumNumberOfSteps(string file, long min, long max, long expected) {
+    public void Test_Runtime_GetNumberOfCollisions(string file, long min, long max, long expected) {
         var sut = new Runtime(file);
 
         Assert.That(sut.GetNumberOfCollisions(min, max), Is.EqualTo(expected));
+    }
+    [TestCase("input.txt", 200000000000000, 400000000000000, 13072)]
+    public void Test_Runtime_GetNumberOfCollisions_GreaterThan(string file, long min, long max, long expected) {
+        var sut = new Runtime(file);
+
+        Assert.That(sut.GetNumberOfCollisions(min, max), Is.GreaterThan(expected));
     }
 
     [TestCase("example-1.txt", 5)]
@@ -33,5 +41,29 @@ public class Tests {
         var sut = new Runtime(file);
 
         Assert.That(sut.hailstones[index].velocity, Is.EqualTo(new Vector3Int(x, y, z)));
+    }
+
+    [TestCase("example-1.txt", 0, 1, 14.333, 15.333)]
+    [TestCase("example-1.txt", 0, 2, 11.667, 16.667)]
+    [TestCase("example-1.txt", 0, 3, 6.2, 19.4)]
+    [TestCase("example-1.txt", 1, 3, -6, -5)]
+    [TestCase("example-1.txt", 2, 3, -2, 3)]
+    public void Test_Runtime_TryCalculateIntersection_Success(string file, int a, int b, double x, double y) {
+        var sut = new Runtime(file);
+
+        Assert.That(Runtime.TryCalculateIntersection(sut.hailstones[a], sut.hailstones[b], out var actual), Is.True);
+        Assert.That(actual.x, Is.EqualTo(x).Within(ERROR_MARGIN));
+        Assert.That(actual.y, Is.EqualTo(y).Within(ERROR_MARGIN));
+    }
+
+    [TestCase("example-1.txt", 0, 4)] // Hailstones' paths crossed in the past for hailstone A.
+    [TestCase("example-1.txt", 1, 2)] // Hailstones' paths are parallel; they never intersect.
+    [TestCase("example-1.txt", 1, 4)] // Hailstones' paths crossed in the past for both hailstones.
+    [TestCase("example-1.txt", 2, 4)] // Hailstones' paths crossed in the past for hailstone B.
+    [TestCase("example-1.txt", 3, 4)] // Hailstones' paths crossed in the past for both hailstones.
+    public void Test_Runtime_TryCalculateIntersection_Failure(string file, int a, int b) {
+        var sut = new Runtime(file);
+
+        Assert.That(Runtime.TryCalculateIntersection(sut.hailstones[a], sut.hailstones[b], out _), Is.False);
     }
 }
