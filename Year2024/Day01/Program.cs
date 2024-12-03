c6cf3592920175acf5394f09898a4cd8a43709e8
Daniel Schulz <faulolio@gmail.com>
2024-12-03 18:56:22 +0100

    feat: day 1

--- Year2024/Day01/Program.cs
+++ Year2024/Day01/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Day01;
 
-Console.WriteLine($"{nameof(Runtime.calibrationSum)}: {new Runtime("example-1.txt").calibrationSum}");
+Console.WriteLine($"{nameof(Runtime.totalDistance)}: {new Runtime("example-1.txt").totalDistance}");
 
-Console.WriteLine($"{nameof(Runtime.calibrationSum)}: {new Runtime("input.txt").calibrationSum}");
+Console.WriteLine($"{nameof(Runtime.totalDistance)}: {new Runtime("input.txt").totalDistance}");
 
-Console.WriteLine($"{nameof(Runtime.calibrationSum)}: {new Runtime("example-2.txt", true).calibrationSum}");
-
-Console.WriteLine($"{nameof(Runtime.calibrationSum)}: {new Runtime("input.txt", true).calibrationSum}");
--- Year2024/Day01/Tests.cs
+++ Year2024/Day01/Tests.cs
@@ -4,28 +4,14 @@ namespace Day01;
 
 [TestFixture(TestOf = typeof(Runtime))]
 public class Tests {
-    [TestCase("1abc2", 12)]
-    [TestCase("pqr3stu8vwx", 38)]
-    [TestCase("a1b2c3d4e5f", 15)]
-    [TestCase("treb7uchet", 77)]
-    public void GivenLine_ThenFindDigits(string input, int expected) {
-        Assert.That(Runtime.FindCalibration(input), Is.EqualTo(expected));
+    [TestCase("example-1.txt", 11)]
+    public void TestExample(string file, int expected) {
+        Assert.That(new Runtime(file).totalDistance, Is.EqualTo(expected));
     }
 
-    [TestCase("two1nine", 29)]
-    [TestCase("eightwothree", 83)]
-    [TestCase("abcone2threexyz", 13)]
-    [TestCase("xtwone3four", 24)]
-    [TestCase("4nineeightseven2", 42)]
-    [TestCase("zoneight234", 14)]
-    [TestCase("7pqrstsixteen", 76)]
-    public void GivenLine_WhenFindWord_ThenFindDigits(string input, int expected) {
-        Assert.That(Runtime.FindCalibration(input, true), Is.EqualTo(expected));
-    }
-
-    [TestCase("example-1.txt", 142, false)]
-    [TestCase("example-2.txt", 281, true)]
-    public void GivenFile_WhenGetCalibrationSum_ThenReturn(string file, int expected, bool findWords) {
-        Assert.That(new Runtime(file, findWords).calibrationSum, Is.EqualTo(expected));
+    [TestCase(3, 4, 1)]
+    [TestCase(7, 2, 5)]
+    public void TestDelta(int left, int right, int expected) {
+        Assert.That(Runtime.Delta(left, right), Is.EqualTo(expected));
     }
 }
