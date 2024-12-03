f8bf928a28db9c0280cf2a3cf55eef69fb3cfd63
Daniel Schulz <faulolio@gmail.com>
2024-12-03 20:01:33 +0100

    refactor: disable 2023/23b

--- Year2023/Day23/Program.cs
+++ Year2023/Day23/Program.cs
@@ -6,4 +6,4 @@ Console.WriteLine($"{nameof(Runtime.maximumNumberOfSteps)} input.txt: {new Runti
 
 Console.WriteLine($"{nameof(Runtime.maximumNumberOfSteps)} example-1.txt: {new Runtime("example-1.txt", true).maximumNumberOfSteps}");
 
-Console.WriteLine($"{nameof(Runtime.maximumNumberOfSteps)} input.txt: {new Runtime("input.txt", true).maximumNumberOfSteps}");
+// Console.WriteLine($"{nameof(Runtime.maximumNumberOfSteps)} input.txt: {new Runtime("input.txt", true).maximumNumberOfSteps}");
