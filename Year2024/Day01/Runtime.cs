667c06bc370797830d7a5c389317ca97c144aa99
Daniel Schulz <faulolio@gmail.com>
2024-12-03 19:25:43 +0100

    feat: solve 1/1

--- Year2024/Day01/Runtime.cs
+++ Year2024/Day01/Runtime.cs
@@ -1,4 +1,5 @@
-﻿using Utilities;
+﻿using System.Text.RegularExpressions;
+using Utilities;
 
 namespace Day01;
 
@@ -6,13 +7,32 @@ sealed class Runtime {
     internal readonly List<int> left = [];
     internal readonly List<int> right = [];
 
+    readonly Regex numberExpression = new("\\d+");
+
     internal Runtime(string file) {
         foreach (string line in new FileInput(file).ReadLines()) {
-            string[] numbers = line.Split(' ');
+            var matches = numberExpression.Matches(line);
+            left.Add(int.Parse(matches[0].Value));
+            right.Add(int.Parse(matches[1].Value));
         }
     }
 
     internal static int Delta(int left, int right) => Math.Abs(left - right);
 
-    internal int totalDistance => 0;
+    internal int totalDistance {
+        get {
+            int sum = 0;
+
+            while (left.Count > 0) {
+                int leftMin = left.Min();
+                left.Remove(leftMin);
+                int rightMin = right.Min();
+                right.Remove(rightMin);
+
+                sum += Delta(leftMin, rightMin);
+            }
+
+            return sum;
+        }
+    }
 }
--- Year2024/Day01/Tests.cs
+++ Year2024/Day01/Tests.cs
@@ -5,8 +5,19 @@ namespace Day01;
 [TestFixture(TestOf = typeof(Runtime))]
 public class Tests {
     [TestCase("example-1.txt", 11)]
+    [TestCase("input.txt", 1603498)]
     public void TestExample(string file, int expected) {
         Assert.That(new Runtime(file).totalDistance, Is.EqualTo(expected));
+    }
+
+    [TestCase("example-1.txt", 3, 4, 2, 1, 3, 3)]
+    public void TestExample_Left(string file, params int[] expected) {
+        Assert.That(new Runtime(file).left, Is.EqualTo(expected));
+    }
+
+    [TestCase("example-1.txt", 4, 3, 5, 3, 9, 3)]
+    public void TestExample_Right(string file, params int[] expected) {
+        Assert.That(new Runtime(file).right, Is.EqualTo(expected));
     }
 
     [TestCase(3, 4, 1)]
