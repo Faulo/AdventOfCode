7e5c927579911be338aa0134a0429fa6652d3118
Daniel Schulz <faulolio@gmail.com>
2024-12-07 00:25:26 +0100

    feat: 6a

---
+++ Year2024/Day06/Program.cs
@@ -0,0 +1,6 @@
+﻿using Day06;
+
+Console.WriteLine($"{nameof(Runtime.pathCount)}: {new Runtime("example-1.txt").pathCount}");
+
+Console.WriteLine($"{nameof(Runtime.pathCount)}: {new Runtime("input.txt").pathCount}");
+
---
+++ Year2024/Day06/Runtime.cs
@@ -0,0 +1,53 @@
+﻿using Utilities;
+
+namespace Day06;
+
+sealed partial class Runtime {
+
+    internal readonly CharacterMap map;
+
+    internal Runtime(string file) {
+        map = new FileInput(file).ReadAllAsCharacterMap();
+    }
+
+    static readonly Dictionary<char, Vector2Int> directions = new() {
+        ['^'] = Vector2Int.up,
+        ['v'] = Vector2Int.down,
+        ['<'] = Vector2Int.left,
+        ['>'] = Vector2Int.right,
+
+    };
+
+    static readonly Dictionary<char, char> ninetyDegreesRight = new() {
+        ['^'] = '>',
+        ['v'] = '<',
+        ['<'] = '^',
+        ['>'] = 'v',
+    };
+
+    int CalculatePath(Vector2Int position, char direction) {
+        var positions = new HashSet<Vector2Int>();
+
+        while (map.IsInBounds(position)) {
+            positions.Add(position);
+
+            while (map.TryGet(position + directions[direction], out char tile) && tile == '#') {
+                direction = ninetyDegreesRight[direction];
+            }
+
+            position += directions[direction];
+        }
+
+        return positions.Count;
+    }
+
+    internal int pathCount {
+        get {
+            var (position, character) = map
+                .allPositionsAndCharactersWithin
+                .First(tile => directions.ContainsKey(tile.character));
+
+            return CalculatePath(position, character);
+        }
+    }
+}
