9a1a8de2662ad85863f27f501a274e3dcff504a6
Daniel Schulz <faulolio@gmail.com>
2024-12-07 01:27:16 +0100

    feat: 6b

--- Year2024/Day06/Program.cs
+++ Year2024/Day06/Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine($"{nameof(Runtime.pathCount)}: {new Runtime("example-1.txt").p
 
 Console.WriteLine($"{nameof(Runtime.pathCount)}: {new Runtime("input.txt").pathCount}");
 
+Console.WriteLine($"{nameof(Runtime.obstructionCount)}: {new Runtime("example-1.txt").obstructionCount}");
+
+Console.WriteLine($"{nameof(Runtime.obstructionCount)}: {new Runtime("input.txt").obstructionCount}");
+
--- Year2024/Day06/Tests.cs
+++ Year2024/Day06/Tests.cs
@@ -9,4 +9,10 @@ public class Tests {
     public void PathCount(string file, int expected) {
         Assert.That(new Runtime(file).pathCount, Is.EqualTo(expected));
     }
+
+    [TestCase("example-1.txt", 6)]
+    [TestCase("input.txt", 1575)]
+    public void ObstructionCount(string file, int expected) {
+        Assert.That(new Runtime(file).obstructionCount, Is.EqualTo(expected));
+    }
 }
