18345b67573ae68d4e506e3e65fc7113c78521ea
Daniel Schulz <faulolio@gmail.com>
2024-12-08 14:51:48 +0100

    refactor: improve performance

--- Year2024/Day07/Runtime.cs
+++ Year2024/Day07/Runtime.cs
@@ -3,80 +3,94 @@
 namespace Day07;
 
 sealed partial class Runtime {
+    readonly struct Row {
+        internal readonly long result;
+        readonly long[] operands;
 
-    internal readonly (long result, long[] operands)[] rows;
+        internal Row(long result, long[] operands) {
+            this.result = result;
+            this.operands = operands;
+        }
 
-    internal Runtime(string file) {
-        rows = new FileInput(file)
-            .ReadLines()
-            .Select(row => row.Split(": "))
-            .Select(row => (long.Parse(row[0]), row[1].Split(' ').Select(long.Parse).ToArray()))
-            .ToArray();
-    }
+        internal bool canBeTrue {
+            get {
+                int length = (int)Math.Pow(2, operands.Length - 1);
 
-    internal static bool CanBeTrue(long result, long[] operands) {
-        int length = (int)Math.Pow(2, operands.Length - 1);
+                for (int i = 0; i < length; i++) {
+                    long test = operands[0];
 
-        for (int i = 0; i < length; i++) {
+                    for (int j = 1; j < operands.Length; j++) {
+                        test = ((i >> (j - 1)) & 1) switch {
+                            0 => test * operands[j],
+                            1 => test + operands[j],
+                            _ => 0,
+                        };
+
+                        if (test == 0) {
+                            return false;
+                        }
+                    }
+
+                    if (test == result) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        internal bool canBeThree {
+            get {
+                int length = (int)Math.Pow(4, operands.Length - 1);
+
+                return Enumerable
+                    .Range(0, length)
+                    .Any(CanBeThreeInternal);
+            }
+        }
+
+        bool CanBeThreeInternal(int i) {
             long test = operands[0];
 
             for (int j = 1; j < operands.Length; j++) {
-                test = ((i >> (j - 1)) & 1) switch {
-                    0 => test * operands[j],
-                    1 => test + operands[j],
-                    _ => 0,
-                };
+                int op = (i >> (2 * (j - 1))) & 3;
 
-                if (test == 0) {
-                    return false;
+                switch (op) {
+                    case 0:
+                        test *= operands[j];
+                        break;
+                    case 1:
+                        test += operands[j];
+                        break;
+                    case 2:
+                        test = long.Parse(test.ToString() + operands[j]);
+                        break;
+                    default:
+                        return false;
                 }
             }
 
-            if (test == result) {
-                return true;
-            }
+            return test == result;
         }
-
-        return false;
     }
 
-    internal static bool CanBeThree(long result, long[] operands) {
-        int length = (int)Math.Pow(4, operands.Length - 1);
+    readonly string file;
 
-        return Enumerable
-            .Range(0, length)
-            .Any(i => CanBeThreeInternal(result, operands, i));
-    }
+    ParallelQuery<Row> rows => new FileInput(file)
+        .ReadLines()
+        .AsParallel()
+        .Select(row => row.Split(": "))
+        .Select(row => new Row(long.Parse(row[0]), row[1].Split(' ').Select(long.Parse).ToArray()));
 
-    static bool CanBeThreeInternal(long result, long[] operands, int i) {
-        long test = operands[0];
-
-        for (int j = 1; j < operands.Length; j++) {
-            int op = (i >> (2 * (j - 1))) & 3;
-
-            switch (op) {
-                case 0:
-                    test *= operands[j];
-                    break;
-                case 1:
-                    test += operands[j];
-                    break;
-                case 2:
-                    test = long.Parse(test.ToString() + operands[j]);
-                    break;
-                default:
-                    return false;
-            }
-        }
-
-        return test == result;
+    internal Runtime(string file) {
+        this.file = file;
     }
 
     internal long sumOfTrue {
         get {
             return rows
-                .AsParallel()
-                .Where(row => CanBeTrue(row.result, row.operands))
+                .Where(row => row.canBeTrue)
                 .Sum(row => row.result);
         }
     }
@@ -84,9 +98,12 @@ sealed partial class Runtime {
     internal long sumOfThree {
         get {
             return rows
-                .AsParallel()
-                .Where(row => CanBeThree(row.result, row.operands))
+                .Where(row => row.canBeThree)
                 .Sum(row => row.result);
         }
     }
+
+    internal static bool CanBeTrue(long result, long[] operands) => new Row(result, operands).canBeTrue;
+
+    internal static bool CanBeThree(long result, long[] operands) => new Row(result, operands).canBeThree;
 }
