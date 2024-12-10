569769acc6385ff8ba728fb045548e374f1cbc0b
Daniel Schulz <faulolio@gmail.com>
2024-12-10 04:02:10 +0100

    feat: 9a

--- Year2024/Day09/Runtime.cs
+++ Year2024/Day09/Runtime.cs
@@ -2,16 +2,88 @@
 
 namespace Day09;
 
-sealed partial class Runtime {
-    readonly string manifest;
+sealed partial class Runtime(string file) {
+    internal readonly struct HDD {
+        internal readonly List<Block> blocks = [];
 
-    internal Runtime(string file) {
-        manifest = new FileInput(file).ReadLines().First();
+        internal HDD(string manifest) {
+            int id = 0;
+            bool isFile = true;
+            for (int i = 0; i < manifest.Length; i++) {
+                blocks.Add(new(isFile ? id++ : -1, int.Parse(manifest[i..(i + 1)])));
+
+                isFile = !isFile;
+            }
+        }
+
+        internal void Defrag() {
+            for (int i = 0; i < blocks.Count; i++) {
+                var space = blocks[i];
+                if (space.isFree) {
+                    var file = blocks[^1];
+                    int delta = space.size - file.size;
+                    switch (delta) {
+                        case 0:
+                            space.id = file.id;
+                            file.id = -1;
+                            break;
+                        case < 0:
+                            // space is smaller than file, split file
+                            space.id = file.id;
+                            file.size = Math.Abs(delta);
+                            break;
+                        case > 0:
+                            // space is larger than file, split space
+                            blocks.Insert(i, new(file.id, file.size));
+                            space.size = delta;
+                            file.id = -1;
+                            break;
+                    }
+
+                    while (blocks[^1].isFree) {
+                        blocks.RemoveAt(blocks.Count - 1);
+                    }
+                }
+            }
+        }
+
+        internal long checksum {
+            get {
+                long i = 0;
+                long sum = 0;
+                foreach (var block in blocks) {
+                    for (int j = 0; j < block.size; j++, i++) {
+                        if (!block.isFree) {
+                            sum += i * block.id;
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
     }
+
+    internal sealed record Block {
+        internal int id;
+        internal int size;
+        internal bool isFree => id < 0;
+
+        internal Block(int id, int size) {
+            this.id = id;
+            this.size = size;
+        }
+
+        public override string ToString() => $"{id}: {size}";
+    }
+
+    internal HDD hdd = new(new FileInput(file).ReadLines().First());
 
     internal long defragChecksum {
         get {
-            return 0;
+            hdd.Defrag();
+
+            return hdd.checksum;
         }
     }
 
--- Year2024/Day09/Tests.cs
+++ Year2024/Day09/Tests.cs
@@ -8,4 +8,33 @@ sealed class Tests {
     public void DefragChecksum(string file, long expected) {
         Assert.That(new Runtime(file).defragChecksum, Is.EqualTo(expected));
     }
+
+    [TestCase("12345", 0, 1, -1, 2, 1, 3, -1, 4, 2, 5)]
+    public void Blocks(string manifest, params int[] blocks) {
+        var expected = new List<Runtime.Block>();
+        for (int i = 0; i < blocks.Length; i += 2) {
+            expected.Add(new(blocks[i], blocks[i + 1]));
+        }
+
+        Assert.That(new Runtime.HDD(manifest).blocks, Is.EqualTo(expected));
+    }
+
+    [TestCase("12345", 0, 1, 2, 2, 1, 3, 2, 3)]
+    public void Defrag(string manifest, params int[] blocks) {
+        var expected = new List<Runtime.Block>();
+        for (int i = 0; i < blocks.Length; i += 2) {
+            expected.Add(new(blocks[i], blocks[i + 1]));
+        }
+
+        var sut = new Runtime.HDD(manifest);
+
+        sut.Defrag();
+
+        Assert.That(sut.blocks, Is.EqualTo(expected));
+    }
+
+    [TestCase("12345", 0 + 3 + 4 + 5 + 20 + 22 + 24 + 26 + 28)]
+    public void TestChecksum(string manifest, long expected) {
+        Assert.That(new Runtime.HDD(manifest).checksum, Is.EqualTo(expected));
+    }
 }
