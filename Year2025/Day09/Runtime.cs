f8a87ede3fce7457fb0730369d6aad8ccc010912
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-12-09 16:11:33 +0100

    test(day09): b

--- Year2025/Day09/Runtime.cs
+++ Year2025/Day09/Runtime.cs
@@ -5,7 +5,40 @@ namespace Day09;
 sealed partial class Runtime {
     readonly List<Vector2Int> redTiles = [];
 
+    bool IsRed(Vector2Int position) => redTiles.Contains(position);
+
+    internal bool IsGreenOrRed(Vector2Int position) {
+        if (IsRed(position)) {
+            return true;
+        }
+
+        bool isInside = false;
+        for (int i = 0, j = redTiles.Count - 1; i < redTiles.Count; i++) {
+            var start = redTiles[j];
+            var end = redTiles[i];
+            if ((start.y < position.y && end.y >= position.y) || (end.y < position.y && start.y >= position.y)) {
+                if (start.x <= position.x || end.x <= position.x) {
+                    isInside = !isInside;
+                }
+            }
+
+            j = i;
+        }
+
+        return isInside;
+    }
+
     IEnumerable<(Vector2Int left, Vector2Int right)> uniquePairs {
+        get {
+            for (int i = 0; i < redTiles.Count; i++) {
+                for (int j = i + 1; j < redTiles.Count; j++) {
+                    yield return (redTiles[i], redTiles[j]);
+                }
+            }
+        }
+    }
+
+    IEnumerable<(Vector2Int left, Vector2Int right)> uniquePairsOnlyGreen {
         get {
             for (int i = 0; i < redTiles.Count; i++) {
                 for (int j = i + 1; j < redTiles.Count; j++) {
@@ -26,5 +59,8 @@ sealed partial class Runtime {
     }
 
     internal long largestRectangleArea => uniquePairs
+        .Max(pair => Vector2Int.RectangleArea(pair.left, pair.right));
+
+    internal long largestRectangleAreaOnlyGreen => uniquePairsOnlyGreen
         .Max(pair => Vector2Int.RectangleArea(pair.left, pair.right));
 }
--- Year2025/Day09/Tests.cs
+++ Year2025/Day09/Tests.cs
@@ -8,4 +8,26 @@ sealed class Tests {
     public void Test_largestRectangleArea(string file, long expected) {
         Assert.That(new Runtime(file).largestRectangleArea, Is.EqualTo(expected));
     }
+
+    [TestCase("example.txt", 7, 0, false)]
+    [TestCase("example.txt", 6, 1, false)]
+    [TestCase("example.txt", 7, 1, true)]
+    [TestCase("example.txt", 8, 1, true)]
+    [TestCase("example.txt", 9, 1, true)]
+    [TestCase("example.txt", 10, 1, true)]
+    [TestCase("example.txt", 11, 1, true)]
+    [TestCase("example.txt", 12, 1, false)]
+    [TestCase("example.txt", 7, 2, true)]
+    [TestCase("example.txt", 8, 2, true)]
+    [TestCase("example.txt", 9, 2, true)]
+    [TestCase("example.txt", 10, 2, true)]
+    [TestCase("example.txt", 11, 2, true)]
+    public void Test_IsGreenOrRed(string file, int x, int y, bool expected) {
+        Assert.That(new Runtime(file).IsGreenOrRed(new(x, y)), Is.EqualTo(expected));
+    }
+
+    [TestCase("example.txt", 24)]
+    public void Test_largestRectangleAreaOnlyGreen(string file, long expected) {
+        Assert.That(new Runtime(file).largestRectangleAreaOnlyGreen, Is.EqualTo(expected));
+    }
 }
